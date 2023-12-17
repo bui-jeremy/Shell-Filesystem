8c999644d0d048d40090b8ccb50c1ab5a929752e
Chen Yang <1007167017@qq.com>
2023-12-16 22:16:47 -0500

    I undersdand all code in module.c and simplified all comments in it

--- kernel/module.c
+++ kernel/module.c
@@ -1,7 +1,3 @@
-/*
- *  ioctl test module -- Rich West.
- */
-
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/errno.h> /* error codes */
@@ -15,90 +11,66 @@
 
 MODULE_LICENSE("GPL");
 
-/* This function is the initialize function of the ramdisk. */
+/* Declaration of module entry(initialization function) and exit */
 void ramdisk_init(void);
-/* This function free the 2MB memory of the ramdisk allocated by call the vmalloc function. */
 void ramdisk_uninit(void);
-/* This function return the length of the directory entry data structure, which is the value 16. */
+
+/* return the length of the directory entry data structure, which is the value 16. */
 int ramdisk_get_dir_entry_length(void);
 
-/* This is the main entry point of the kernel module's ioctl function. */
+/* Declaration of ioctl function*/
 static int rd_ioctl(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_creat function
-   to create a regular file. */
+/* Declaration of all operating functions */
 static int rd_creat(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_unlink function
-   to remove the file from the ramdisk with absolute
-   pathname from the filesystem, freeing its memory in the ramdisk. */
 static int rd_unlink(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_open function
-   to open an existing file corresponding to pathname. */
 static int rd_open(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_close function
-   to close the corresponding file. */
 static int rd_close(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_read function
-   to read up to num_bytes from a regular file
-   to the specified address in the calling process. */
 static int rd_read(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_write function
-   to write up to num_bytes from the specified address
-   in the calling process to a regular file. */
 static int rd_write(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_lseek function
-   to set the file object's file position
-   identified by index node number index_node_number, to offset. */
 static int rd_lseek(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_mkdir function
-   to read one entry from a directory file identified by
-   index node number index_node_number, and store the result
-   in memory at the specified value of address. */
 static int rd_mkdir(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function call ramdisk kernel module's ramdisk_open function
-to open an existing file corresponding to pathname. */
 static int rd_readdir(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg);
 
-/* This function get the pathname from the calling process. */
-char *strdup_ramdisk(pathname_t *pathname);
+/* Copy path(string) from user space */
+char *strdup_ramdisk(pathname_t *pathname);   
 
 
-static struct file_operations pseudo_dev_proc_operations;
+static struct file_operations pseudo_dev_proc_operations;   // Define operations in /proc
+static struct proc_dir_entry *proc_entry;                   // Pointer used to create the entry in /proc
 
-static struct proc_dir_entry *proc_entry;
-
-
+/* Module entry point initialization */
 static int __init initialization_routine(void) {
   pseudo_dev_proc_operations.ioctl = rd_ioctl;
 
-  /* Start create proc entry */
-  proc_entry = create_proc_entry("ramdisk", 0444, NULL);
+  proc_entry = create_proc_entry("ramdisk", 0444, NULL);    //create proc entry
   if(!proc_entry)
   {
     printk("<1> Error creating /proc entry.\n");
     return 1;
   }
 
-  //proc_entry->owner = THIS_MODULE; <-- This is now deprecated
-  proc_entry->proc_fops = &pseudo_dev_proc_operations;
+  /*Set the entry's file operation function pointer proc_fops to pseudo_dev_proc_operations 
+  so that ioctl calls will be processed correctly.*/
+  proc_entry->proc_fops = &pseudo_dev_proc_operations;   
   ramdisk_init();
 
   return 0;
@@ -127,66 +99,44 @@ static void __exit cleanup_routine(void) {
 }
 
 
-/* This is the main entry point of the kernel module's ioctl function. */
+/* The main entry point of the kernel module's ioctl function. */
 static int rd_ioctl(struct inode *inode, struct file *file,
 				unsigned int cmd, unsigned long arg)
 {
   switch (cmd)
   {
-  /* Call ramdisk kernel module's ramdisk_creat function
-     to create a regular file. */
-  case IOCTL_CREAT:
+  case IOCTL_CREAT:      //cmd --> IOCTL_CREAT
     rd_creat(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_unlink function
-     to remove the file from the ramdisk with absolute
-     pathname from the filesystem, freeing its memory in the ramdisk. */
   case IOCTL_UNLINK:
     rd_unlink(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_open function
-     to open an existing file corresponding to pathname. */
   case IOCTL_OPEN:
     rd_open(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_close function
-     to close the corresponding file. */
   case IOCTL_CLOSE:
     rd_close(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_read function
-     to read up to num_bytes from a regular file
-     to the specified address in the calling process. */
   case IOCTL_READ:
     rd_read(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_write function
-     to write up to num_bytes from the specified address
-     in the calling process to a regular file. */
   case IOCTL_WRITE:
     rd_write(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_lseek function
-     to set the file object's file position
-     identified by index node number index_node_number, to offset. */
   case IOCTL_LSEEK:
     rd_lseek(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_mkdir function
-     to create a directory. */
   case IOCTL_MKDIR:
     rd_mkdir(inode, file, cmd, arg);
     break;
 
-  /* Call ramdisk kernel module's ramdisk_open function
-     to open an existing file corresponding to pathname. */
   case IOCTL_READDIR:
     rd_readdir(inode, file, cmd, arg);
     break;
@@ -199,33 +149,28 @@ static int rd_ioctl(struct inode *inode, struct file *file,
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_creat function
-   to create a regular file. */
+/* The following functions correspond to various operations in ioctl */
 static int rd_creat(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
-  creat_param_t creat_param;
+  creat_param_t creat_param;  //int return_value;  pathname_t pathname;
   char *pathname = NULL;
 
   copy_from_user(&creat_param, (creat_param_t *)arg,
-    sizeof(creat_param_t));
-  /* Get the pathname from the calling process. */
-  pathname = strdup_ramdisk(&creat_param.pathname);
+    sizeof(creat_param_t));  //Copy data from user space
+  
+  pathname = strdup_ramdisk(&creat_param.pathname);  //Copy path from user space
 
-  /* Call ramdisk kernel module's ramdisk_creat function
-     to create a regular file. */
-  creat_param.return_value = ramdisk_creat(pathname);
-  copy_to_user((int *)arg, &creat_param.return_value, sizeof(int));
+  creat_param.return_value = ramdisk_creat(pathname);  //Call the corresponding operation of the kernel module and use the result as the return value
+  copy_to_user((int *)arg, &creat_param.return_value, sizeof(int));   //Copy results back to user space
 
-  /* Free the pathname. */
+  //Free the pathname. 
   kfree(pathname);
 
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_unlink function
-   to remove the file from the ramdisk with absolute
-   pathname from the filesystem, freeing its memory in the ramdisk. */
+/* remove the file from the ramdisk */
 static int rd_unlink(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
@@ -234,23 +179,16 @@ static int rd_unlink(struct inode *inode, struct file *file,
 
   copy_from_user(&unlink_param, (creat_param_t *)arg,
     sizeof(creat_param_t));
-  /* Get the pathname from the calling process. */
   pathname = strdup_ramdisk(&unlink_param.pathname);
-
-  /* Call ramdisk kernel module's ramdisk_unlink function
-     to remove the file from the ramdisk with absolute
-     pathname from the filesystem, freeing its memory in the ramdisk. */
   unlink_param.return_value = ramdisk_unlink(pathname);
   copy_to_user((int *)arg, &unlink_param.return_value, sizeof(int));
 
-  /* Free the pathname. */
   kfree(pathname);
 
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_open function
-   to open an existing file corresponding to pathname. */
+/*Open an existing file corresponding to pathname. */
 static int rd_open(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
@@ -259,22 +197,17 @@ static int rd_open(struct inode *inode, struct file *file,
 
   copy_from_user(&open_param, (open_param_t *)arg,
     sizeof(open_param_t));
-  /* Get the pathname from the calling process. */
   pathname = strdup_ramdisk(&open_param.pathname);
 
-  /* Call ramdisk kernel module's ramdisk_open function
-     to open an existing file corresponding to pathname. */
   open_param.return_value = ramdisk_open(pathname, &open_param.index_node_number);
   copy_to_user((open_param_t *)arg, &open_param, sizeof(open_param_t));
 
-  /* Free the pathname. */
   kfree(pathname);
 
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_close function
-   to close the corresponding file. */
+/* Close the corresponding file. */
 static int rd_close(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
@@ -283,16 +216,13 @@ static int rd_close(struct inode *inode, struct file *file,
   copy_from_user(&close_param, (close_param_t *)arg,
     sizeof(close_param_t));
 
-  /* Call ramdisk kernel module's ramdisk_close function
-     to close the corresponding file. */
   close_param.return_value = ramdisk_close(close_param.index_node_number);
   copy_to_user((int *)arg, &close_param.return_value, sizeof(int));
 
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_read function
-   to read up to num_bytes from a regular file
+/* Read up to num_bytes from a regular file
    to the specified address in the calling process. */
 static int rd_read(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
@@ -302,9 +232,6 @@ static int rd_read(struct inode *inode, struct file *file,
   copy_from_user(&read_param, (read_write_param_t *)arg,
     sizeof(read_write_param_t));
 
-  /* Call ramdisk kernel module's ramdisk_read function
-     to read up to num_bytes from a regular file
-     to the specified address in the calling process. */
   read_param.return_value = ramdisk_read(read_param.index_node_number,
     read_param.file_position,
     read_param.address,
@@ -314,8 +241,7 @@ static int rd_read(struct inode *inode, struct file *file,
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_write function
-   to write up to num_bytes from the specified address
+/* Write up to num_bytes from the specified address
    in the calling process to a regular file. */
 static int rd_write(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
@@ -325,9 +251,6 @@ static int rd_write(struct inode *inode, struct file *file,
   copy_from_user(&write_param, (read_write_param_t *)arg,
     sizeof(read_write_param_t));
 
-  /* Call ramdisk kernel module's ramdisk_write function
-     to write up to num_bytes from the specified address
-     in the calling process to a regular file. */
   write_param.return_value = ramdisk_write(write_param.index_node_number,
     write_param.file_position,
     write_param.address,
@@ -337,9 +260,7 @@ static int rd_write(struct inode *inode, struct file *file,
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_lseek function
-   to set the file object's file position
-   identified by index node number index_node_number, to offset. */
+/* set the file object's read & write position(offset). */
 static int rd_lseek(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
@@ -349,9 +270,6 @@ static int rd_lseek(struct inode *inode, struct file *file,
   copy_from_user(&lseek_param, (lseek_param_t *)arg,
     sizeof(lseek_param_t));
 
-  /* Call ramdisk kernel module's ramdisk_lseek function
-     to set the file object's file position
-     identified by index node number index_node_number, to offset. */
   lseek_param.return_value = ramdisk_lseek(lseek_param.index_node_number,
     lseek_param.seek_offset,
     &seek_result_offset);
@@ -364,8 +282,7 @@ static int rd_lseek(struct inode *inode, struct file *file,
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_mkdir function
-   to create a directory. */
+/* Create a directory. */
 static int rd_mkdir(struct inode *inode, struct file *file,
   unsigned int cmd, unsigned long arg)
 {
@@ -374,22 +291,17 @@ static int rd_mkdir(struct inode *inode, struct file *file,
 
   copy_from_user(&mkdir_param, (creat_param_t *)arg,
     sizeof(creat_param_t));
-  /* Get the pathname from the calling process. */
   pathname = strdup_ramdisk(&mkdir_param.pathname);
 
-  /* Call ramdisk kernel module's ramdisk_mkdir function
-     to create a directory. */
   mkdir_param.return_value = ramdisk_mkdir(pathname);
   copy_to_user((int *)arg, &mkdir_param.return_value, sizeof(int));
 
-  /* Free the pathname. */
   kfree(pathname);
 
   return 0;
 }
 
-/* This function call ramdisk kernel module's ramdisk_mkdir function
-   to read one entry from a directory file identified by
+/* Read one entry from a directory file identified by
    index node number index_node_number, and store the result
    in memory at the specified value of address. */
 static int rd_readdir(struct inode *inode, struct file *file,
@@ -400,10 +312,6 @@ static int rd_readdir(struct inode *inode, struct file *file,
   copy_from_user(&readdir_param, (readdir_param_t *)arg,
     sizeof(readdir_param_t));
 
-  /* Call ramdisk kernel module's ramdisk_mkdir function
-     to read one entry from a directory file identified by
-     index node number index_node_number, and store the result
-     in memory at the specified value of address. */
   readdir_param.return_value = ramdisk_readdir(readdir_param.index_node_number,
     readdir_param.address,
     &readdir_param.file_position);
@@ -417,7 +325,7 @@ static int rd_readdir(struct inode *inode, struct file *file,
   return 0;
 }
 
-/* This function get the pathname from the calling process. */
+/* This function get the pathname from the calling process(user space). */
 char *strdup_ramdisk(pathname_t *pathname)
 {
   char *dup_str = NULL;
